28e150caa3917c60dec35f24de31799da6d4a5a8
suraksha-poojary <surakshapoojary853@gmail.com>
2024-06-10 12:01:18 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect,useState } from 'react'
+
 
 function App() {
+  const[isUsers,setIsUsers]=useState(undefined);
+  useEffect(()=>{
+    let Task=async()=>{
+      let response= await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
+
+   setIsUsers(response.data); 
+  
+  } 
+  Task()
+},[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {isUsers&&<div>{isUsers.time.updated}</div>}
+      </div>
   );
 }
 
